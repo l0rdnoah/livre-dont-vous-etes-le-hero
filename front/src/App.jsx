4fac281678d7a09aab222668e9e1673ba82eb0ba
l0rdnoah <noah.brohan@etudiant.univ-rennes1.fr>
2024-04-02 15:52:55 +0200

    btnchoixaddrafronansaitpascoder

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,11 +3,11 @@ import './App.css';
 import Menu from './component/menu_principal/menu.jsx';
 import BarreVie from './component/barre_vie/barre_vie.jsx';
 import BarreEndurance from './component/barre_endurance/barre_endurance.jsx';
-
+import BoutonChoix from './component/bouton_choix/bouton_choix.jsx';
 function App() {
   // Définir des valeurs pour la vie actuelle et la vie maximale
-  const vieActuelle = 400;
-  const vieMax = 500;
+  const vieActuelle = 200;
+  const vieMax = 200;
   const enduranceActuelle = 400;
   const enduranceMax = 500;
 
@@ -16,7 +16,8 @@ function App() {
       {/* <Menu /> */}
       <BarreVie vieActuelle={vieActuelle} vieMax={vieMax} />
       <BarreEndurance enduranceActuelle={enduranceActuelle} enduranceMax={enduranceMax} />
+      <BoutonChoix /> 
     </>
-  );
+  )
 }
 export default App;
---
+++ front/src/component/bouton_choix/bouton_choix.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './bouton_choix.css';
+
+function BoutonChoix({ vieActuelle, vieMax }) {
+    return (
+        <>  
+        <a href="">
+            <div className="conteneurBouton">
+                <p className="contenuBouton">Aller à la section 1</p>
+            </div>
+        </a>
+        </>
+    );
+}
+
+export default BoutonChoix;
