6c40ff9441f11cf86816281675fdac2956ef8cda
huleguen <hugo.leguen@outlook.com>
2024-04-03 15:10:11 +0200

    appel persos

--- front/src/pages/charger/charger.jsx
+++ front/src/pages/charger/charger.jsx
@@ -5,45 +5,34 @@ import './charger.css';
 function Charger() {
   document.title = "Charger";
 
-  const persos = [
-    {
-    id: 2,
-    id_utilisateur: 25,
-    section_actuelle: 0,
-    nom: "toto",
-    endurance: 400,
-    habilite: 20,
-    po: 5,
-    modif_degats: 0,
-    endurance_max: 500,
-    },
-    {
-      id: 2,
-      id_utilisateur: 25,
-      section_actuelle: 0,
-      nom: "toto",
-      endurance: 400,
-      habilite: 20,
-      po: 5,
-      modif_degats: 0,
-      endurance_max: 500,
-    },
-    {
-      id: 2,
-      id_utilisateur: 25,
-      section_actuelle: 0,
-      nom: "toto",
-      endurance: 400,
-      habilite: 20,
-      po: 5,
-      modif_degats: 0,
-      endurance_max: 500,
-    },
-  ];
+  const [texte, setTexte] = useState('');
 
+
+  const fetchData = async () => {
+    try {
+      sessionStorage.setItem('id_utilisateur', 12);
+      console.log('test')
+      const id_utilisateur = sessionStorage.getItem('id_utilisateur');
+      const response = await fetch(`http://localhost:3000/api/utilisateur/${id_utilisateur}/personnages`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const persos = await response.json();
+      setTexte(data[0]['texte']);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setTexte("Erreur de chargement des personnages");
+    }
+  };
+
+  fetchData();
+  
   return (
       <div id="wrapper">
         <img id="logo" src={logo} alt="" />
+        <div className="text">
+          {texte}
+        </div>
         <div>
           {persos.map((personnage, index) => (
             <ChargerPersonnage key={index} personnage={personnage} />
