2f3ade4aeb60244cf5aa55a3af31bc093d29d2cc
Ronan <ronan.bragato@gmail.com>
2024-04-03 13:40:56 +0200

    NOAHCBONJAUTORISE

--- back/app.js
+++ back/app.js
@@ -8,7 +8,13 @@ const routeTest = require('./routers/routeTest.js')
 const routeSection = require('./routers/routeSection.js')
 const routeCombat = require('./routers/routeCombat.js')
 
-//
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+  
 
 app.use('/api/test', routeTest)
 app.use('/api/section', routeSection)
