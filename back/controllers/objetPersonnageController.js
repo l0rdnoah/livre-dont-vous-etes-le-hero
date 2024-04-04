366f59d24fbc18bf773f7317ecdd310b260e9e5a
Paul Favreau <paul44favreau@gmail.com>
2024-04-04 23:55:56 +0200

    verif sur objet deja en base

--- back/controllers/objetPersonnageController.js
+++ back/controllers/objetPersonnageController.js
@@ -9,17 +9,29 @@ exports.addObjetToPersonnage = async (req, res) => {
     const idObjet= req.query.idObjet;
     const idPersonnage= req.query.idPersonnage;
 
-    try{
+    try {
+        const existingAssociation = await models.Objet_Personnage.findOne({
+            where: {
+                id_personnage: idPersonnage,
+                id_objet: idObjet
+            }
+        });
+    
+        
+        if (existingAssociation) {
+            return res.status(409).json({ message: 'Cette association existe déjà' });
+        }
+    
+        
         await models.Objet_Personnage.create({
             id_personnage: idPersonnage,
             id_objet: idObjet
-        })
-
-        return res.status(200).json({message: 'Objet ajouté au personnage'})
-    }
-    catch(error){
-        console.log('erreur lors de addObjetToPersonnage: '+error)
-        res.status(500).json({error: 'Erreur lors de l\'ajout de l\'objet au personnage'})
+        });
+    
+        return res.status(200).json({ message: 'Objet ajouté au personnage' });
+    } catch (error) {
+        console.log('erreur lors de addObjetToPersonnage: ' + error);
+        res.status(500).json({ error: 'Erreur lors de l\'ajout de l\'objet au personnage' });
     }
 }
 
