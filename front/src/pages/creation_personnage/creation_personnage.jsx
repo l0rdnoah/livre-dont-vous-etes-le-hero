16bd05fdb5cfc7dabd222b3a53d915c8eed20a40
huleguen <hugo.leguen@outlook.com>
2024-04-04 09:42:09 +0200

    fix chargement partie + creation perso

--- front/src/pages/creation_personnage/creation_personnage.jsx
+++ front/src/pages/creation_personnage/creation_personnage.jsx
@@ -1,6 +1,7 @@
 import './creation_personnage.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+
 import logo from '../../assets/logo_v1.png';
 import Des from '../../component/des/Des';
 
@@ -8,51 +9,108 @@ function CreationPersonnage() {
 
     document.title = "Création du personnage";
     const [nom, setNom] = useState('');
-    const [nombreDes, setNombreDes] = useState('');
-    const [resultatDes, setResultatDes] = useState('');
-
-    const [lancerDesEndurance, setLancerDesEndurance] = useState('');
-    const [lancerDesHabilite, setLancerDesHabilite] = useState('');
+    const [boutonEnabled, setBoutonEnabled] = useState(true);
     const [resultatDesEndurance, setResultatDesEndurance] = useState('lancer de dés');
     const [resultatDesHabilite, setResultatDesHabilite] = useState('lancer de dés');
+    const [section, setSection] = useState(1);
+    const navigate = useNavigate();
 
-    const lancerDeDes = async (e) => {
-        try {
-          
+    const getResDesEndurance = (value) => {
+        setBoutonEnabled(false);
+        setResultatDesEndurance(value);
+    };
+
+    const getResDesHabilite = (value) => {
+        setBoutonEnabled(false);
+        setResultatDesHabilite(value);
+    };
+
+    const incrementeSection = () => {
+        setBoutonEnabled(true);
+        setSection(section+1);
+    };
+
     
+    // Fonction pour créer un personnage
+    const creerPersonnage = async (e) => {
+        e.preventDefault();
+
+        const params = new URLSearchParams({
+            idUtilisateur: sessionStorage.getItem('id_utilisateur'),
+            nom: nom,
+            habilite: 3+resultatDesHabilite,
+            endurance: 20+resultatDesEndurance,
+            modifDegats: 0,
+            enduranceMax: 20+resultatDesEndurance
+        });
+
+        try {
+            const response = await fetch(`http://localhost:3000/api/personnage/creerpersonnage?${params.toString()}`);
+            const data = await response.json();
+
+            if (response.ok) {
+                console.log('Personnage créé avec succès:', data);
+                navigate(`/Jeu?idSection=1&idPersonnage=${data.id}`);
+            } else {
+                console.error('Erreur lors de la création du personnage:', data);
+            }
         } catch (error) {
-          console.error('Erreur lors de la connexion:', error);
-          setErrorMessage('Erreur lors de la connexion.');
+            console.error('Erreur lors de la création du personnage:', error);
         }
     };
 
     return (
-        <>
-        <Des nbdes={2} setresdes={}></Des>        
+        <>        
         <div id="wrapper">
             <img id="logo" src={logo} alt="" />
-            <div className='creation_personnage'>
-                <form id="form">
-                    <div className="champ">
-                        <label>
-                        Nom du personnage :
-                        </label>
-                        <input
-                        type="text"
-                        value={nom}
-                        onChange={(e) => setNom(e.target.value)}
-                        />
-                    </div>
-                    <div className='champ'>
-                        <label>Endurance max : 20 + ({resultatDesEndurance})</label>
-                        <button onClick={lancerDeDes}>Lancer les dés</button>
-                    </div>
-                    <div className='champ'>
-                        <label>Habilité : 3 + ({resultatDesHabilite}) </label>
-                        <button>Lancer un dé</button>
-                    </div>
-                    <button type="submit">Créer le personnage</button>
+            <div className='creerPersonnage'>
+                <form onSubmit={creerPersonnage} id="form">
+                    {section === 1 && (
+                        <div className="champ">
+                            <label>
+                            Nom du personnage :
+                            </label>
+                            <input
+                            type="text"
+                            value={nom}
+                            onChange={(e) => setNom(e.target.value)}
+                            />
+                            {nom !== '' && (
+                                <button onClick={incrementeSection}>Suivant</button>
+                            )}
+                        </div>
+                        
+                    )}
+                    {section === 2 && (
+                        <div className='champ'>
+                            <Des nbdes={2} setresdes={getResDesEndurance} setBoutonEnabled={setBoutonEnabled} boutonenabled={boutonEnabled}></Des>
+                            <label>Endurance max : 20 + ({resultatDesEndurance})</label>
+                            {resultatDesEndurance !== 'lancer de dés' && (
+                                <button onClick={incrementeSection}>Suivant</button>
+                            )}
+                        </div>
+                    )}
+                    {section === 3 && (
+                        <div className='champ'>
+                            <Des nbdes={1} setresdes={getResDesHabilite} setBoutonEnabled={setBoutonEnabled} boutonenabled={boutonEnabled}></Des>
+                            <label>Habilité : 3 + ({resultatDesHabilite}) </label>
+                            {resultatDesHabilite !== 'lancer de dés' && (
+                                <button onClick={incrementeSection}>Suivant</button>
+                            )}                        
+                        </div>
+                    )}
+                    {section === 4 && (
+                        <div>
+                            <p>Nom : {nom}</p>
+                            <p>Endurance max : {20+resultatDesEndurance}</p>
+                            <p>Habilité : {3+resultatDesHabilite}</p>
+                            <button type="submit">Créer le personnage</button>
+                        </div>
+                    )}
                 </form>
+                <Link to={`/Menu`}>
+                    <button>{"Retourner au menu"}</button>
+                </Link>  
             </div>
         </div>
         </>
