39674ae492362465562c87574109d42b1568ef58
cleomc <cleomarinmarie@gmail.com>
2024-04-04 17:36:54 +0200

    belles images inventaire

--- front/src/component/Inventaire/PopUpInventaire.jsx
+++ front/src/component/Inventaire/PopUpInventaire.jsx
@@ -1,35 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import './PopUpInventaire.css';
-
+import React, { useState, useEffect } from "react";
+import "./PopUpInventaire.css";
 
 const getImage = async (id) => {
-  var image = await import(`../../assets/img/itemsInventaire/${id}.png`);
-  return image;
+  const image = await import(`../../assets/img/itemsInventaire/${id}.png`);
+  return image.default;
 };
 
 const PopUpInventaire = (props) => {
+  const [imageUrls, setImageUrls] = useState({});
+
+  useEffect(() => {
+    const loadImageUrls = async () => {
+      const urls = await Promise.all(
+        props.items.map(async (item) => ({
+          id: item.id,
+          url: await getImage(item.id),
+        }))
+      );
+
+      const urlsMap = urls.reduce((acc, curr) => {
+        acc[curr.id] = curr.url;
+        return acc;
+      }, {});
+
+      setImageUrls(urlsMap);
+    };
+
+    loadImageUrls();
+  }, [props.items]);
+
   const fermer = () => {
     props.onFermer();
   };
 
   return (
-    <div className={`inventaire-popup ${props.ouvert ? 'i-ouvert' : 'i-ferme'}`}>
+    <div
+      className={`inventaire-popup ${props.ouvert ? "i-ouvert" : "i-ferme"}`}
+    >
       <div className="inventaire-container">
         <div className="topbarre">
           <h3 className="titre">Inventaire</h3>
-          <div
-            className="fermer"
-            onClick={() => {
-              fermer();
-            }}
-          >
+          <div className="fermer-container">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
+              width="45"
+              height="45"
               fill="currentColor"
-              className="bi bi-x-lg"
+              className="bi bi-x-lg fermer"
               viewBox="0 0 16 16"
+              onClick={fermer}
             >
               <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
             </svg>
@@ -40,7 +59,7 @@ const PopUpInventaire = (props) => {
             <div className="item" key={index}>
               <img
                 className="img-item"
-                src={getImage(item.id)}
+                src={imageUrls[item.id]}
                 alt={item.nom}
                 title={item.nom}
                 width={150}
