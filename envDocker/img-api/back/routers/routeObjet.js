7a2f5c59ba9cfc3afbc2dd70745b23eb1e1d1d99
l0rdnoah <noah.brohan@etudiant.univ-rennes1.fr>
2024-04-05 15:04:10 +0200

    là on est ok bdd + back

--- envDocker/img-api/back/routers/routeObjet.js
+++
@@ -1,7 +0,0 @@
-const express=require('express')
-const router=express.Router()
-const  objet= require('../controllers/objetController')
-
-router.get('/:id/getObjetsByPersonnageId', objet.getObjetsByPersonnageId)
-router.get('/getAllInfoObjetById', objet.getAllInfoObjetById)
-module.exports=router 
--- envDocker/img-api/back/routers/routeSection.js
+++
@@ -1,9 +0,0 @@
-const express=require('express')
-const router=express.Router()
-const  section= require('../controllers/sectionController')
-
-
-router.get('/getallinfosectionbyid', section.getAllInfoSectionById);
-
-
-module.exports=router
